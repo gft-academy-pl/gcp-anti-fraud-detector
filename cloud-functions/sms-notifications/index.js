496e07dd1422a900ff7e7f334ba54c35807db3e2
Piotr Kosmowski <kospiotr@tlen.pl>
2018-04-22 00:42:50 +0200

    Update

--- cloud-functions/sms-notifications/index.js
+++ cloud-functions/sms-notifications/index.js
@@ -1,51 +1,16 @@
-const {
-	google
-} = require('googleapis');
 const fs = require("fs");
 const path = require('path');
-
 const CONFIG = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: CONFIG.GCP_NEXMO_KEY,
+  apiSecret: CONFIG.GCP_NEXMO_SECRET
+});
 
-exports.triggerDataflowFn = (event) => {
-
-	console.log('Processing incomming event');
-
+exports.sendSms = (event) => {
 	const file = event.data;
-
-	return google.auth.getClient({
-		keyFile: path.join(__dirname, 'jwt.keys.json'),
-		scopes: [
-			'https://www.googleapis.com/auth/cloud-platform',
-			'https://www.googleapis.com/auth/userinfo.email'
-		]
-	}).then((auth) => {
-		return google.auth.getDefaultProjectId()
-			.then((projectId) => {
-				console.log(`Succesfully authorized with JWT key, project: ${projectId}`);
-
-				return google.dataflow({
-						version: 'v1b3',
-						auth: auth
-					})
-					.projects.templates.create({
-						projectId: projectId,
-						resource: {
-							parameters: {
-								inputFile: `gs://${file.bucket}/${file.name}`,
-								output: `${CONFIG.OUTPUT_BUCKET}/frauds-${file.name}`
-							},
-							jobName: 'fraud-detector-input-data-triggering-dataflow-' + new Date().toISOString(),
-							gcsPath: CONFIG.GS_PATH
-						}
-					}).then((result) => {
-						
-						console.log('Result: ', result);
-						return result;
-						
-					}, (err) => console.log('Exception occurred while executing DataFlow job template', err.message));
-
-			}, (err) => console.log('Exception occurred while obtaining default projectId', err.message));
-
-	}, (err) => console.log('Exception occurred while obtaining credentials', err.message));
-
+	const from = 'GFT-ACADEMY';
+	const to = CONFIG.GCP_NEXMO_TO;
+	const text = `All details can be found in gs://${file.bucket}/${file.name}`;
+	return nexmo.message.sendSms(from, to, text)	
 };
