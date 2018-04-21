66adac3138159b96face9cbe882e5b44fed512e3
przemyslawjuszkiewicz <przemyslaw.juszkiewicz@gmail.com>
2018-04-21 22:27:49 +0200

    tests

--- cloud-functions/email-notifications/email.test.js
+++ cloud-functions/email-notifications/email.test.js
@@ -1,13 +1,14 @@
 const assert = require('assert');
-const {
-	google
-} = require('googleapis');
+const fs = require("fs");
 const path = require('path');
 
 describe('Sending e-mail', function() {
 
-	it('should send it via API', async function() {
-		
+	it('should load config.json and contains properties', async function() {
+		const CONFIG = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+		assert.ok(CONFIG.GCP_SENDGRID_API_KEY);
+		assert.ok(CONFIG.GCP_FROM_EMAIL);
+		assert.ok(CONFIG.GCP_TO_EMAIL);
 	})
 
 });
