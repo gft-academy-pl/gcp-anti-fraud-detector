d11b328ea21e791835b8534477851c1333a10a2e
Piotr Kosmowski <kospiotr@tlen.pl>
2018-04-22 15:03:40 +0200

    Update authorizedDataFlowApiCallWithJWT.test.js

--- cloud-functions/dataflow-notifications/authorizedDataFlowApiCallWithJWT.test.js
+++ cloud-functions/dataflow-notifications/authorizedDataFlowApiCallWithJWT.test.js
@@ -25,6 +25,7 @@ describe('Dataflow API', function() {
 				}).then(result => {
 					assert.equal(result.data.type, 'JOB_TYPE_BATCH');
 					console.log('result', result.data);
+					return result.data;
 				});
 			});
 		}).then(done);
