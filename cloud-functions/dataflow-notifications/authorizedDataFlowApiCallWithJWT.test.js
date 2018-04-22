fc918e4683e635d1ad02732146f36dcb104478d3
Piotr Kosmowski <kospiotr@tlen.pl>
2018-04-22 23:46:58 +0200

    Update authorizedDataFlowApiCallWithJWT.test.js

--- cloud-functions/dataflow-notifications/authorizedDataFlowApiCallWithJWT.test.js
+++ cloud-functions/dataflow-notifications/authorizedDataFlowApiCallWithJWT.test.js
@@ -7,28 +7,21 @@ const path = require('path');
 const fs = require("fs");
 const CONFIG = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 
-describe('Dataflow API', function() {
-
-	it('should list all jobs using JWT', function(done) {
-		google.auth.getClient({
-			keyFile: path.join(__dirname, 'jwt.keys.json'),
-			scopes: [
-				'https://www.googleapis.com/auth/cloud-platform',
-				'https://www.googleapis.com/auth/userinfo.email'
-			]
-		}).then(auth => {
-			return google.auth.getDefaultProjectId().then(projectId => {
-				console.log('ProjectId', projectId);
-				return index.createJob(auth, projectId, {
-					bucket: CONFIG.INPUT_BUCKET,
-					name: 'trades-small.csv'
-				}).then(result => {
-					assert.equal(result.data.type, 'JOB_TYPE_BATCH');
-					console.log('result', result.data);
-					return null;
-				});
-			});
-		}).then(done);
-	})
-
+google.auth.getClient({
+	keyFile: path.join(__dirname, 'jwt.keys.json'),
+	scopes: [
+		'https://www.googleapis.com/auth/cloud-platform',
+		'https://www.googleapis.com/auth/userinfo.email'
+	]
+}).then(auth => {
+	return google.auth.getDefaultProjectId().then(projectId => {
+		console.log('ProjectId', projectId);
+		return index.createJob(auth, projectId, {
+			bucket: CONFIG.INPUT_BUCKET,
+			name: 'trades-small.csv'
+		}).then(result => {
+			assert.equal(result.data.type, 'JOB_TYPE_BATCH');
+			console.log('result', result.data);
+		}, err => console.log(err));
+	});
 });
